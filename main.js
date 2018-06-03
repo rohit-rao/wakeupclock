b82b7f49fe8abbd1726fb4c034cd17bea7677f82
Rohit Rao <rohit-rao@users.noreply.github.com>
2018-06-02 21:41:24 -0400

    Publish 6f5f65f4a4abf0ef9a49c575144fb134f6732366 to gh-pages.

--- main.js
+++ main.js
@@ -9,6 +9,13 @@ var g_state = DISCONNECTED;
 var g_device;
 var g_characteristic;
 
+function logError(msg) {
+  var container = document.getElementById("section_error_messages");
+  var node = document.createElement("div");
+  node.className = "error";
+  node.innerText = msg;
+  container.appendChild(node);
+}
 
 function onLoad() {
   var connectionButton = document.getElementById("connection");
@@ -23,6 +30,8 @@ function onLoad() {
   document.getElementById("refresh").addEventListener("click", onRefresh);
   document.getElementById("off").addEventListener("click", onTurnLightOff);
   document.getElementById("on").addEventListener("click", onTurnLightOn);
+  document.getElementById("morning_0730").addEventListener(
+      "click", onSetMorningAlarm);
 }
 
 function setState(state) {
@@ -57,6 +66,8 @@ function setState(state) {
   } else {
     document.getElementById("section_clock_state").style.display = "none";
     document.getElementById("section_clock_control").style.display = "none";
+    document.getElementById("section_clock_control_morning").style.display =
+        "none";
     document.getElementById("refresh").style.display = "none";
   }
 
@@ -86,7 +97,7 @@ function startBTConnection() {
           return true;
         })
       .catch(error => {
-          console.log(error);
+          logError(error);
           setState(ERROR);
         });
 }
@@ -106,7 +117,7 @@ function onBTConnectionEstablished(characteristic) {
 
 function onBTDisconnected(event) {
   let device = event.target;
-  console.log('Device ' + device.name + ' is disconnected.');
+  logError('Device ' + device.name + ' is disconnected.');
   setState(DISCONNECTED);
   g_device = undefined;
 }
@@ -116,13 +127,13 @@ function onClockStatusUpdated() {
 
   var response = event.target.value;
   if (response.byteLength != 10) {
-    console.log("Improper response size");
+    logError("Improper response size");
     return;
   }
 
   var status = response.getUint8(0);
   if (status != 0) {
-    console.log("Error response received");
+    logError("Error response received");
     return;
   }
 
@@ -148,6 +159,8 @@ function onClockStatusUpdated() {
 
   document.getElementById("section_clock_state").style.display = "inline";
   document.getElementById("section_clock_control").style.display = "inline";
+  document.getElementById("section_clock_control_morning").style.display =
+      "inline";
 }
 
 function disconnectBT() {
@@ -165,7 +178,7 @@ function onRefresh() {
           return true;
         })
       .catch(error => {
-          console.log(error);
+          logError(error);
           document.getElementById("refresh").disabled = false;
         });
 }
@@ -175,7 +188,7 @@ function onTurnLightOff() {
   // light off immediately.
   var command = Uint8Array.of(0x79, 0x44, 12, 0, 0, 15, 0x7A);
   g_characteristic.writeValue(command)
-      .catch(error => { console.log(error); });
+      .catch(error => { logError(error); });
 }
 
 function onTurnLightOn() {
@@ -189,7 +202,17 @@ function onTurnLightOn() {
       enableTime.getHours(), enableTime.getMinutes(),
       30, 15, 0x7A);
   g_characteristic.writeValue(command)
-      .catch(error => { console.log(error); });
+      .catch(error => { logError(error); });
+}
+
+function onSetMorningAlarm() {
+  // Send a command to set the morning alarm time to 7:30am.
+  var command = Uint8Array.of(
+      0x79, 0x43,
+      7, 30,
+      0, 15, 0x7A);
+  g_characteristic.writeValue(command)
+      .catch(error => { logError(error); });
 }
 
 function formatTime(hours, minutes) {
