e4012ae1643323e6b776aeff4dae22cb0781a20d
Rohit Rao <rohit-rao@users.noreply.github.com>
2019-05-18 13:45:54 -0400

    Publish dfefedbf85c188b6c0d49d94b5a8027b680cd890 to gh-pages

---
+++ sw.js
@@ -0,0 +1,42 @@
+var RESOURCE_VERSION = 1;
+
+var URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/main.css',
+  '/main.js'
+]
+
+function getCacheName() {
+  return "wakeupclock-v" + RESOURCE_VERSION;
+}
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(getCacheName()).then(function (cache) {
+      return cache.addAll(URLS_TO_CACHE);
+    }));
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  // delete any old caches
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key != getCacheName()) {
+          return caches.delete(key);
+        }
+      })
+    )).then(() => {
+      console.log('V' + RESOURCE_VERSION + ' now ready to handle fetches!');
+    })
+  );
+});
